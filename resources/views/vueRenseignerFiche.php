f52f7486b769f335ed7daa09f77d6c5084058354
stevenhamotshilling <stevenhamotshillingford@leschassagnes.net>
2020-11-25 17:29:30 +0100

    Ajout fonction warning et erased

--- resources/views/vueRenseignerFiche.php
+++ resources/views/vueRenseignerFiche.php
@@ -5,19 +5,19 @@
         <input type="number" name="nuitee" id="nuitee">
     </form>
     <form action="" method="get">
-        <label for="repasMidi">Saisir frais du repas de midi</label>
+        <label for="repasMidi">Saisir frais du repas de midi : </label>
         <input type="number" name="repasMidi" id="repasMidi">
     </form>
     <form action="" method="get">
-        <label for="kilometrage">Saisir frais du kilométrage</label>
+        <label for="kilometrage">Saisir frais du kilométrage : </label>
         <input type="number" name="kilometrage" id="kilometrage">
     </form>
     <form action="" method="get"> <!-- Facultatif (si l'utilisateur n'a pas de frais supplementaire il doit pouvoir sauvegarder en laissant vide)-->
-        <label for="fraisSupp">Saisir frais supplementaire</label>
+        <label for="fraisSupp">Saisir frais supplementaire : </label>
         <input type="number" name="fraisSupp" id="fraisSupp">
     </form>
     <form action="" method="get"> <!-- Facultatif (si l'utilisateur n'a pas de frais supplementaire il doit pouvoir sauvegarder en laissant vide)-->
-        <label for="fraisSuppLib">Saisir libellé du frais supplementaire</label>
+        <label for="fraisSuppLib">Saisir libellé du frais supplementaire : </label>
         <input type="text" name="fraisSuppLib" id="fraisSuppLib">
     </form>
     <button type="submit">Sauvegarder</button>
